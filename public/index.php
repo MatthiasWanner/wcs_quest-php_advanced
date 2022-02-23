f0cd6fdff62e6a62c1be57ba5bd9432cbc90aa30
MatthiasWanner <matthias.wanner@gmail.com>
2022-02-23 16:24:15 +0100

    :sparkles: (quest) composer ques #1 => Wcs namespace with Hello classe

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
 use App\Learn\Foo;
+use App\Wcs\Hello;
 require __DIR__ . '/../vendor/autoload.php';
 
 $foo = new Foo('Hello');
+$wcs = new Hello();
 ?>
 
 <?= $foo->classInfos() ?>
+
+</br>
+
+<?= $wcs->talk() ?>
