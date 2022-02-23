329fa67f1968b38bebd0c38d11d57a194473f1dc
MatthiasWanner <matthias.wanner@gmail.com>
2022-02-23 16:27:21 +0100

    :fire: (quest) Delete Foo class to prepare composer quest#1 solution

--- public/index.php
+++ public/index.php
@@ -1,14 +1,9 @@
 <?php
-use App\Learn\Foo;
 use App\Wcs\Hello;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-$foo = new Foo('Hello');
 $wcs = new Hello();
 ?>
-
-<?= $foo->classInfos() ?>
-
-</br>
 
 <?= $wcs->talk() ?>
