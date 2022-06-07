fa4b501ec6ba14c4125994b0d46c80b510df707c
MatthiasWanner <matthias.wanner@gmail.com>
2022-06-07 13:35:44 +0200

    :sparkles: (template) Try new prodtcs template using Twig variables

---
+++ public/products.php
@@ -0,0 +1,10 @@
+<?php
+// Get a $twig object from this file.
+require_once __DIR__ . '/../config/twig.php';
+
+
+$name = 'Matthias';
+$message = 'How are you today ?';
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+echo $twig->render('products.html.twig', ['name' => $name, 'message' => $message, 'products' => $products]);
