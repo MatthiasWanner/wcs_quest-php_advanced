7944abe89c72f0788de1d33032f37c94e21eb084
MatthiasWanner <matthias.wanner@gmail.com>
2022-06-07 14:09:45 +0200

    :lipstick: (ui) Split array into group of 3 products thanks to twig syntax

--- public/products.php
+++ public/products.php
@@ -5,6 +5,6 @@ require_once __DIR__ . '/../config/twig.php';
 
 $name = 'Matthias';
 $message = 'How are you today ?';
-$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe', 'ukulele'];
 
 echo $twig->render('products.html.twig', ['name' => $name, 'message' => $message, 'products' => $products]);
